7607329defdca627811509af039c9208dd876f48
Ivy Nguyen <animeweirdo24@gmail.com>
2022-09-16 20:03:52 -0600

    mmm

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ app.use(
   })
 )
 
-// app.get('/', (request, response) => {
-//     response.json( db.getSongs )
-//   })
+app.get('/', db.getSongs )
 
-  app.get('/api/v1/songs', db.getSongs)
+  // app.get('/api/v1/songs', db.getSongs)
 
   app.listen(port, () => {
     console.log(`App running on port ${port}.`)
