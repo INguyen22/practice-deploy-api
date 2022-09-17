adfbc19e6a4f1f259efdafb304a452eb34878d98
Ivy Nguyen <animeweirdo24@gmail.com>
2022-09-17 12:21:08 -0600

    try 2

--- api/product.js
+++ api/product.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const db = require('./queries')
 
 router.get("/", async (req, res) => {
     try {
-        res.json({
-            status: 200,
-            message: 'WORK PLEASE'
-        });
+        db.getSongs
     } catch (error) {
         console.error(error)
         return res.status(500).send("sever boo")
