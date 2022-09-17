37e0bc4eb0f415d2c92b23b2f710fa0c51439c2f
Ivy Nguyen <animeweirdo24@gmail.com>
2022-09-16 19:15:11 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3001
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  app.get('/songs', db.getSongs)
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
+  // Export the Express API
+module.exports = app;
---
+++ queries.js
@@ -0,0 +1,44 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'user1',
+  host: 'localhost',
+  database: 'onehitwonders',
+  password: 'password',
+  port: 5432,
+})
+
+  const getSongs = (request, response) => {
+    pool.query('SELECT * FROM songs ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      const newObject = results.rows.reduce((obj, currentSong) => {
+        if(!obj[currentSong.decade]) {
+          obj[currentSong.decade] = []
+        }
+        obj[currentSong.decade].push(currentSong)
+        return obj
+      }, {})
+      response.status(200).json(newObject)
+    })
+  }
+
+  module.exports = {
+    getSongs,
+  }
+
+//   const getSongs = (request, response) => {
+//     pool.query('SELECT * FROM songs ORDER BY id ASC', (error, results) => {
+//       if (error) {
+//         throw error
+//       }
+//       const newObject = results.rows.reduce((obj, currentSong) => {
+//         if(!obj[currentSong.decade]) {
+//           obj[currentSong.decade] = []
+//         }
+//         obj[currentSong.decade].push(currentSong)
+//         return obj
+//       }, {})
+//       response.status(200).json(newObject)
+//     })
+//   }
